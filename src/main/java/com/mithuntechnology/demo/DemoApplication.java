6c10fdd8dea7c7ca0ba70eaeec0de332d5c9a6ac
praka07 <prakashbtw.007@gmail.com>
2020-02-18 10:07:17 +0530

    refer logo in html file

--- src/main/java/com/mithuntechnology/demo/DemoApplication.java
+++ src/main/java/com/mithuntechnology/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.mithuntechnology.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/**").addResourceLocations(
+	            "classpath:/static/","classpath:/image/");
 	}
 
 }
